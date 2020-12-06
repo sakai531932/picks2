053865a3f10abf10dbd7fc18c31054be346feb0a
”酒井” <sakai531932@gmail.com>
2020-12-06 03:19:08 +0000

    paykoushin

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,13 @@ Rails.application.routes.draw do
   
   get 'talk_list/:user_id' => 'relationships#talk_list', as: 'talk_list'
   
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+  
   
 end
